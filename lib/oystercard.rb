460555949e9738c0826d51e78bcee776e7bde661
Nimzyow <n_soufiani@hotmail.com>
2020-02-24 15:37:19 +0000

    added private method to raise error if max balance is exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,12 @@ class Oystercard
   end
 
   def top_up(money)
+    check_max_balance(money)
     @balance += money
+  end
+
+  private
+  def check_max_balance(money)
+    raise "top_up exceeds maximum balance" if @balance + money > 90
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,17 @@ describe Oystercard do
   end
   context "method responds to" do
     it "#top_up" do
-    expect(subject).to respond_to(:top_up).with(1).arguments
+      expect(subject).to respond_to(:top_up).with(1).arguments
+    end
+  end
+  context "method functionality" do
+    it "tops up the balance by given amount" do
+      subject.top_up(3)
+      expect(subject.balance).to eq(3)
+    end
+    it "raises error if #top_up exceeds max balance" do
+      subject.top_up(50)
+      expect{subject.top_up(50)}.to raise_error("top_up exceeds maximum balance")
     end
   end
   
