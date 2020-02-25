b6e0d35b739d463cbbf5bdbb8886f9c9231aec51
Nimzyow <n_soufiani@hotmail.com>
2020-02-25 12:23:03 +0000

    tested for minimum charge, min balance, and set journey to true or false depending on if user touched in or out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,12 @@
 class Oystercard
   MAX_BALANCE = 90
-  attr_accessor :balance
+  MIN_BALANCE = 1
+  MINIMUM_CHARGE = 5
+  attr_accessor :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -12,15 +15,28 @@ class Oystercard
     success_message
   end
 
+
+  def touch_in
+    raise "no money, bruh" if balance < MIN_BALANCE
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MINIMUM_CHARGE)
+    @in_journey = false
+  end
+  private
+
   def deduct(money)
     @balance -= money
   end
 
-  private
   def success_message
     "Succesfully topped up. current balance is #{@balance}"
   end
+
   def check_max_balance(money)
     raise "top_up exceeds £#{MAX_BALANCE}" if @balance + money > MAX_BALANCE
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,35 +1,71 @@
 require "oystercard"
 
+shared_context "common" do
+  let(:top_up_and_enter) {
+    subject.top_up(10)
+    subject.touch_in
+  }
+end
+
 describe Oystercard do
+  include_context "common"
+  
   context "has attributes" do
     it "balance" do
       expect(subject).to have_attributes(balance: 0)
     end
+    it "in_journey" do
+      expect(subject).to have_attributes(in_journey: false)
+    end
   end
+
   context "method responds to" do
     it "#top_up" do
       expect(subject).to respond_to(:top_up).with(1).arguments
     end
-    it "#deduct" do
-      expect(subject).to respond_to(:deduct).with(1).arguments
+    it "touch_in" do
+      expect(subject).to respond_to(:touch_in)
     end
+    it "touch_out" do
+      expect(subject).to respond_to(:touch_out)
+    end
+    
   end
+
   context "method functionality" do
-    it "tops up the balance by given amount" do
+    it "#top_up tops up the balance by given amount" do
       subject.top_up(3)
       expect(subject.balance).to eq(3)
     end
-    it "deducts given amount from balance" do
-      subject.top_up(50)
-      subject.deduct(30)
-      expect(subject.balance).to eq(20)
+    it "#touch_out deducts given amount from balance" do
+      subject.top_up(10)
+      subject.touch_out
+      expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_CHARGE)
     end
     it "raises error if #top_up exceeds max balance" do
       subject.top_up(50)
       expect{subject.top_up(50)}.to raise_error("top_up exceeds £90")
     end
-    it "gives user message of successful top up" do
+    it "#top_up gives user message of successful top up" do
       expect(subject.top_up(50)).to eq("Succesfully topped up. current balance is #{subject.balance}")
+    end
+
+    it "#touch_in switches in_journey to true" do
+      top_up_and_enter
+      expect(subject.in_journey).to be true 
+    end
+    it "#touch_in raises error when balance is < 1" do
+      expect{subject.touch_in}.to raise_error("no money, bruh")
+    end
+    it "#touch_out switched in_journey to false" do
+      top_up_and_enter
+      subject.touch_out
+      expect(subject.in_journey).to be false
+    end
+    it "#touch_out deducts minimum charge of -5" do
+      top_up_and_enter
+      subject.touch_out
+      expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_CHARGE)
     end
   end
   
